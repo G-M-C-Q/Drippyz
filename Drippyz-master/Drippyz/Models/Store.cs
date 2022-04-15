2782de5a4df149d796506e5d3d921b0faa1f28fb
Gillian <100874964+G-M-C-Q@users.noreply.github.com>
2022-04-15 21:32:56 +0100

    Add files via upload
    Models added(not validated) relations established, Db Context file added; need to configure SQL Server

---
+++ Drippyz-master/Drippyz/Models/Store.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Drippyz.Models
+{
+    public class Store
+    {  
+        [Key]
+        public int Id { get; set; }
+        public string Glyph { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //Relationship (Store can have a list of products)
+        public List<Product> Products { get; set; }
+
+    }
+}
